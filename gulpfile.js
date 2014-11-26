021681779a406081f71cf06d82c35ff18c18ca93
Nate Gibbons <nate.gibbons@gmail.com>
2014-11-26 15:34:42 -0500

    moved stuff to app, added bower

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,41 @@ var jade = require('gulp-jade');
 var less = require('gulp-less');
 var del = require('del');
 var vinyl = require('vinyl-paths');
+var bower = require('gulp-bower');
 
 
 gulp.task('clean',function(){
-    gulp.src('dist/**/*')
+    gulp.src('dist/**/*.html')
+        .pipe(vinyl(del));
+
+    gulp.src('dist/css')
         .pipe(vinyl(del));
 });
 
+gulp.task('bower', function(){
+    return bower();
+});
+
 gulp.task('jade', function(){
-    gulp.src('jade/*.jade')
+    gulp.src('app/jade/*.jade')
         .pipe(jade({pretty: true}))
         .pipe(gulp.dest('dist'));
 });
 
 
 gulp.task('lesscss', function(){
-    gulp.src('less/*.less')
+    gulp.src('app/less/*.less')
         .pipe(less())
         .pipe(gulp.dest('dist/css'));
 });
 
 
 gulp.task('default',['clean','jade','lesscss'], function(){
-    gulp.watch('jade/*',['jade']);
-    gulp.watch('less/*',['lesscss']);
+    gulp.watch('app/jade/*',['jade']);
+    gulp.watch('app/less/*',['lesscss']);
 
 });
+
+gulp.task('build', ['clean','bower','jade','lesscss']);
 
 
